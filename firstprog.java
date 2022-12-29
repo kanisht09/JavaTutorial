0a70d269c784baf877e48d0d59e7436214a54454
kanisht09 <kanisht09@gmail.com>
2022-12-29 21:17:51 +0530

    Initial commit

---
+++ firstprog.java
@@ -0,0 +1,7 @@
+public class firstprog {
+    public static void main(String args[]) {
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ printProg.java
@@ -0,0 +1,18 @@
+public class printProg {
+    public static void main(String args[]) {
+
+        /* Print statements in Java */
+
+        System.out.println("Hello"); // prints "Hello" with '/n'
+        System.out.print("Hi"); // prints "Hi" without '/n'
+        System.out.print('\n'); // prints '\n' (a newline character)
+        int x = 5;
+        System.out.println(x); // prints an integer value, x
+        double y = 2.232;
+        System.out.println(y); // prints a double value , y
+
+        String z = "Hello Java";
+        System.out.print(z); // prints a string , z
+
+    }
+}
