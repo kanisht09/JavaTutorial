810cfaf474f33e47428b007b3bc5d3f0df85f4f7
kanisht09 <kanisht09@gmail.com>
2022-12-29 21:24:44 +0530

    Added printing hello world in java

--- firstprog.java
+++ firstprog.java
@@ -1,6 +1,8 @@
 public class firstprog {
     public static void main(String args[]) {
 
+        /* Starting Java Language with printing hello world statement */
+
         System.out.println("Hello World");
 
     }
