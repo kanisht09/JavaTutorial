4d56d98227dcbc13704f5ae141ff5129e292c11c
kanisht09 <kanisht09@gmail.com>
2022-12-31 16:08:57 +0530

    Added Print statement in Java

---
+++ DataTypes.java
@@ -0,0 +1,38 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte age = 5;
+        System.out.println(age); // prints a value of type byte, age
+
+        /* Here 5 is called a literal */
+
+        short k = 5;
+        System.out.println(k); // prints a value of type short, k
+
+        char cq = 'A';
+        System.out.println(cq); // prints a character value , cq
+
+        int x = 5;
+        System.out.println(x); // prints an integer value, x
+
+        /* double is a default literal for decimal numbers */
+
+        double y = 2.232;
+        System.out.println(y); // prints a double value , y
+
+        float yf = 2.232f; // we need to write f in the end of the decimal number
+        System.out.println(yf); // prints a float value, yf
+
+        long kl = (1 << 5);
+        System.out.println(kl); // prints a long value, k
+
+        boolean c = true;
+        System.out.println(c); // prints a boolean value, c
+
+        c = false;
+        System.out.println(c);
+
+        String z = "Hello Java";
+        System.out.println(z); // prints a string , z
+    }
+}
--- printProg.java
+++ printProg.java
@@ -6,16 +6,6 @@ public class PrintProg {
         System.out.println("Hello"); // prints "Hello" with '/n'
         System.out.print("Hi"); // prints "Hi" without '/n'
         System.out.print('\n'); // prints '\n' (a newline character)
-        int x = 5;
-        System.out.println(x); // prints an integer value, x
-        double y = 2.232;
-        System.out.println(y); // prints a double value , y
-
-        String z = "Hello Java";
-        System.out.println(z); // prints a string , z
-
-        long k = (1 << 5); // prints a long value, k
-        System.out.println(k);
 
     }
 }
