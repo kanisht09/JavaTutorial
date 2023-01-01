724cd8dfbd84ebb01a40965f1b3ac7a4fe3dd3f1
kanisht09 <kanisht09@gmail.com>
2023-01-01 18:23:22 +0530

    Added String class and its methods

---
+++ StringMethods.java
@@ -0,0 +1,10 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = new String("harry");
+        System.out.println(name);
+
+        String name2 = "Harry";
+        System.out.println(name2);
+
+    }
+}
