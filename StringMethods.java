e43aff84ab1496f9d4f4ec18c7f7f4cd89823472
kanisht09 <kanisht09@gmail.com>
2023-01-03 01:45:07 +0530

    Added String Methods in java

--- StringMethods.java
+++ StringMethods.java
@@ -39,7 +39,71 @@ public class StringMethods {
 
         name = "I am Harry";
         String replacedName = name.replace('r', 'p');
+
+        /*
+         * replace method replaces all the
+         * occurences of a character or a
+         * string
+         */
+
         System.out.println(replacedName);
+        replacedName = replacedName.replace("ppy", "rry");
+        System.out.println(replacedName);
+
+        name = "Hello";
+        System.out.println(name.startsWith("Hel")); // prints true
+
+        /*
+         * Method name : startsWith()
+         * Parameter : string only (no single character)
+         * Return type : boolean
+         * Description : checks whether the given string starts with the input parameter
+         */
+
+        System.out.println(name.startsWith("Eel")); // prints false
+
+        /*
+         * Method name : endsWith()
+         * Parameter : string only (no single character)
+         * Return type : boolean
+         * Description : checks whether the given string ends with the input parameter
+         */
+
+        System.out.println(name.endsWith("llo")); // prints true
+        System.out.println(name.endsWith("tlo")); // prints false
+
+        // name.charAt(i) is equivalent to name[i] in C/C++ but we are not allowed to
+        // change its value
+
+        name = "Hello";
+
+        System.out.println(name.charAt(1));
+
+        // name.charAt(1)='e'; // this statement is forbidden in Java
+
+        // name.indexOf("string") -> returns the first index of the input parameter
+
+        /* NOTE : indexOf() returns -1 if pattern is not found */
+
+        name = "Helelo"; // first index of "el" is at position 1
+
+        System.out.println(name.indexOf("el"));
+
+        System.out.println(name.indexOf("el", 2)); /* additional parameter where to start searching from */
+
+        // last index of "el" is at position 3
+
+        System.out.println(name.lastIndexOf("el"));
+
+        System.out.println(name.lastIndexOf("el", 2)); /* additional parameter where to start searching from */
+
+        // equals() similar to == method for string comparison in C++
+        // NOTE : In Java you can't compare strings using == operator
+
+        name = "golu";
+        System.out.println(name.equals("golu"));
+        System.out.println(name.equals("Golu"));
+        System.out.println(name.equalsIgnoreCase("Golu")); // ignoring case of characters
 
     }
 }
