8a2f315be7950f3b04c7dcbe928feed4ab0ff21f
kanisht09 <kanisht09@gmail.com>
2023-01-06 21:44:13 +0530

    Added Method overloading in java

---
+++ MethodOverloading.java
@@ -0,0 +1,33 @@
+public class MethodOverloading {
+    
+    static void tellJoke(){
+        System.out.println("I invented a new word\n"+"Plagiarism");
+    }
+    static void change1(int x){ // number gets copied
+        x=55;
+    }
+    static void change2(int [] marks){ // reference gets copied
+        marks[0]=55;
+    }
+    public static void main(String[] args) {
+        tellJoke();
+        
+        // Case 1: changing a number
+
+        int x=65;
+        change1(x);
+        System.out.println(x); // NO change
+
+        // case 2: changing the array
+
+        int [] marks={56,77,86,90,93}; // marks stores the address of the object 
+        change2(marks); // now we are passing the reference or address of the object we created
+        System.out.println(marks[0]); // array element gets changed
+
+
+        /*
+         *  In case of arrays reference is passed and same is the case for objects being passed in methods.
+         */
+
+    }
+}
